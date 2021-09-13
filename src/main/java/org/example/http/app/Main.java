5c8175f95866a1a1765e930f16e53c6f752432d2
twister1803 <mvchekalin@mail.ru>
2021-09-13 21:37:03 +0500

    initial commit

---
+++ src/main/java/org/example/http/app/Main.java
@@ -0,0 +1,31 @@
+package org.example.http.app;
+
+import lombok.extern.java.Log;
+import org.example.http.framework.Server;
+import org.example.http.framework.resolver.argument.RequestHandlerMethodArgumentResolver;
+import org.example.http.framework.resolver.argument.RequestHeaderHandlerMethodArgumentResolver;
+import org.example.http.framework.resolver.argument.ResponseHandlerMethodArgumentResolver;
+
+import java.io.IOException;
+
+@Log
+public class Main {
+  public static void main(String[] args) {
+    final var server = new Server(9999);
+    server.autoRegisterHandlers("org.example.http.app");
+    server.addArgumentResolver(
+        new RequestHandlerMethodArgumentResolver(),
+        new ResponseHandlerMethodArgumentResolver(),
+        new RequestHeaderHandlerMethodArgumentResolver()
+    );
+    new Thread(() -> {
+      try {
+        Thread.sleep(3000);
+        server.stop();
+      } catch (InterruptedException | IOException e) {
+        e.printStackTrace();
+      }
+    }).start();
+    server.listen();
+  }
+}
